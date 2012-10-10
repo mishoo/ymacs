ef05c5edb33dd388113177c5080ccc41543d325f
Mihai Bazon <mihai@bazon.net>
2012-10-10 16:36:33 +0300

    switch to UglifyJS2

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# make sure the "uglifyjs" program exists and is in $PATH for
+# make sure the "uglifyjs2" program exists and is in $PATH for
 # minification to work.
 
 TEMPDIR=`mktemp -d`
@@ -16,10 +16,8 @@ JSFILES=`cat loadfiles.txt`
 rm loadfiles.txt
 
 mkdir ../js-minified
-cat $JSFILES > ../js-minified/all.js
-cd ../js-minified
-uglifyjs $* < all.js > ymacs-min.js
-rm all.js
+uglifyjs2 $JSFILES -c -m --source-map ymacs-min.js.map --source-map-root ../js -o ../js-minified/ymacs-min.js
+mv ymacs-min.js.map ../js-minified
 
 cd $TEMPDIR
 tar zcf ymacs.tar.gz Ymacs
